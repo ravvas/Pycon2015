fdb619233aabdf658b104e9c2667f0067df60c15
Harikrishna Ravva <ravvas@rediffmail.com>
2015-10-02 11:33:31 +0530

    Update processout.sh

--- process output files/processout.sh
+++ process output files/processout.sh
@@ -5,7 +5,7 @@ cat /home/ec2-user/scripts/imroutput/* > emrout1
 sort -k2nr emrout1 > emrout2
 head -25 emrout2 > emrout3
 awk '{$1=$1}1' OFS="," emrout3 > emrout4.csv
-aws s3 cp emrout2 s3://hariemrresults/final$filename
-aws s3 cp emrout4.csv s3://hariemrresults/top25$filename.csv
+aws s3 cp emrout2 s3://<your Bucket>/results/final$filename
+aws s3 cp emrout4.csv s3://<your bucket>/results/top25$filename.csv
 rm emrout*
 rm -r imroutput
